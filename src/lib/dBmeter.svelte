8bb6f55c4303a62f55118ea7c3d336529409cbe7
Salero-tech <jan@autobolli.ch>
2023-08-23 19:23:44 +0200

    started

---
+++ src/lib/dBmeter.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+
+</script>
+
+<style lang="scss">
+    div {
+        display: flex;
+        align-items: start;
+        justify-content: start;
+        flex-direction: row;
+        text-justify: right;
+    }
+
+
+    .holder{
+        flex-grow: 1;
+        align-items: end;
+        justify-content: baseline;
+        flex-direction: column;
+    }
+
+
+
+    .number{
+        font-size: 14vw;
+    }
+    .text {
+        font-size: 10vw;
+        color: purple;
+    }
+
+    #choose {
+        color: gray;
+        background-color: transparent;
+        border-color: transparent;
+
+
+        option {
+            background-color: black;
+            border-color: transparent;
+        }
+    }
+    
+
+</style>
+
+<div class="main">
+    <div class="number">74</div>
+    <div class="holder">
+        <select name="test" id="choose">
+            <option value="0">Leq1s</option>
+            <option value="1">Leq1min</option>
+            <option value="2">Leq5min</option>
+            <option value="3">Leq10min</option>
+            <option value="4">Leq15min</option>
+            <option value="5">Leq60min</option>
+        </select>
+        <div class="text">dBA</div>
+    </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,63 @@
+
+<script lang="ts">
+    import DBmeter from "$lib/dBmeter.svelte";
+</script>
+
+<style lang="scss">
+:global(html){
+    height: 100%;
+}
+:global(body) {
+    background-color: black;
+    display: flex;
+    height: 100%;
+    margin: 0;
+    color: whitesmoke;
+}
+
+.content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    flex: 1;
+
+    
+    .middle {
+        width: 100%;
+        display: flex;
+        align-items: center;
+
+        .line {
+            flex: 1;
+            height: 2px;
+            background-color: whitesmoke;
+        }
+        
+        
+    }
+    .bottom {
+        flex: 1;
+        align-items: start;
+    }
+}
+
+
+</style>
+
+<div class="content">
+    <DBmeter />
+    
+    <div class="middle">
+        <div class="offset">Offset ON</div>
+        <div class="line"/>
+    </div>
+
+    <div class="bottom">
+        <DBmeter />
+        <DBmeter />
+    </div>
+</div>
+    
+
+
